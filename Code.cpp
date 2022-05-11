0f750adf6e4ced2dce60a84218a50d328d39fbdd
Victor Garcia <hdjdududydusu@gmail.com>
2022-05-11 05:58:24 -0300

    Update Code.cpp

--- Code.cpp
+++ Code.cpp
@@ -1,3 +1,7 @@
+// Copyright (c) 2021 Victor Garcia
+// Distributed under the MIT software license, see the accompanying
+// file LICENSE.txt or https://opensource.org/licenses/MIT
+
 #include <iostream>
 
 using namespace std;
